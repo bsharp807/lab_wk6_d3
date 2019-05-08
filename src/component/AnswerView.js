6a54f14267207165d6e0bbd598661f0550a2e388
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-05-08 13:23:45 +0100

    guesser working

--- src/component/AnswerView.js
+++ src/component/AnswerView.js
@@ -4,7 +4,6 @@ const AnswerView = (props) => {
 
   const name = props.name
   const answer = props.answer;
-
   if (name === answer) {
       return <div>Yes! That's the correct answer!</div>
   } else {
