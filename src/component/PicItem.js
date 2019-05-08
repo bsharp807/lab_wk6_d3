1776cdf277d89017274927d4fd285ce1f0c78478
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-05-08 10:39:33 +0100

    pics view updated

--- src/component/PicItem.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-import PicItem from './PicItem.js';
---
+++ src/component/PicsView.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PicItem from './PicItem.js';
+
+const PicsView = (props) => {
+
+  const pictures = props.characters.map((character) => {
+    return <PicItem name={character.name} image={character.image}>
+  });
+
+  return (
+    <div class= "pics-view">
+      {pictures}
+    </div>
+  )
+
+}
+
+export default PicsView;
