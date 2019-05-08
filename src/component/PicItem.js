56ad80f54be1662bcbdefc1ca2501c21f506eb06
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-05-08 13:06:15 +0100

    added a bit of styling

--- src/component/PicItem.js
+++ src/component/PicItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './main.css'
 
 const PicItem = (props) => {
   const image = `/images/${props.image}`
   return (
     <div className= "pic-item">
-      <img src={image} />
+      <img src={image} className='pic'/>
       <p>{props.name}</p>
     </div>
   )
